dd5c6615bac6e2886cb3de8995d13a3da8626783
Viktor Titovich <raffityinet@gmail.com>
2023-01-30 16:43:55 +0300

    Add task5

---
+++ seminar5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1-ое число: ");
+
+int.TryParse(Console.ReadLine(), out int number);
+
+Console.WriteLine("Введите 2-ое число: ");
+
+int.TryParse(Console.ReadLine(), out int number1);
+
+                if ((number == Math.Sqrt(number1)) || (number1 == number*number))//Math.Sqrt(number)) )
+                {
+                Console.WriteLine("Да");
+                }
+                else 
+                Console.WriteLine("Нет ");
