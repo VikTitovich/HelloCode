440ddd2a4cf48871fda587e26936ec3405f43672
Viktor Titovich <raffityinet@gmail.com>
2023-01-30 16:11:14 +0300

    Add task3

---
+++ seminar3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите 1-ое число: ");
+
+                int.TryParse(Console.ReadLine(), out int number);
+
+                Console.WriteLine("Введите 2-ое число: ");
+
+                int.TryParse(Console.ReadLine(), out int number1);
+
+                if (number % number1 == 0)
+                {
+                Console.WriteLine("Кратно");
+                }
+                else 
+                Console.WriteLine("Некратно, остаток " + number % number1);
