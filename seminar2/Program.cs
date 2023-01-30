c5c25a3bb0d641b51e2850a7f53e9ac846b40e8a
Viktor Titovich <raffityinet@gmail.com>
2023-01-30 15:45:35 +0300

    Add task 2

---
+++ seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Random rnd = new Random();
+
+int number = rnd.Next(100,999);
+
+Console.WriteLine(number);
+
+int i = number % 10; //Последняя цифра числа
+
+while (number >= 100)
+{
+    number /=100;
+}
+
+int j = number; //Первая цифра числа
+
+Console.WriteLine(j*10+i);
+
