96a3f7a0455531af5f3e3b0b8f51cf7303c19148
Viktor Titovich <raffityinet@gmail.com>
2023-01-30 16:19:51 +0300

    Add task4

---
+++ seminar4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+
+    int.TryParse(Console.ReadLine(), out int number);
+
+    if ((number % 7 == 0) && (number % 23 ==0))
+                {
+                Console.WriteLine("Да");
+                }
+                else 
+                Console.WriteLine("Нет");
