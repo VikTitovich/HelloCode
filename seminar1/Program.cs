290c4ba8de4f608d9a6ed7edd09f788246a32336
Viktor Titovich <raffityinet@gmail.com>
2023-01-30 14:05:40 +0300

    Add method

---
+++ seminar1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
+int.TryParse(Console.ReadLine(), out int i);
+if (i==1) Console.WriteLine("Понедельник");
+if (i==2) Console.WriteLine("Вторник");
+if (i==3) Console.WriteLine("Среда");
+if (i==4) Console.WriteLine("Четверг");
+if (i==5) Console.WriteLine("Пятница");
+if (i==6) Console.WriteLine("Суббота");
+if (i==7) Console.WriteLine("Воскресенье");
+
+
+
+
