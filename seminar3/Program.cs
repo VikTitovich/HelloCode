1d5c3cc7a31a7d41c4d0c90968b7bb87a6504153
Viktor Titovich <raffityinet@gmail.com>
2023-01-31 14:03:10 +0300

    Add code

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,15 +1,21 @@
-﻿
-Console.WriteLine("Введите 1-ое число: ");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
 
-                int.TryParse(Console.ReadLine(), out int number);
 
-                Console.WriteLine("Введите 2-ое число: ");
+Console.WriteLine("Введите первое число: ");
 
-                int.TryParse(Console.ReadLine(), out int number1);
+    int.TryParse(Console.ReadLine(), out int number);
+
+        Console.WriteLine("Введите второе число: ");
+
+            int.TryParse(Console.ReadLine(), out int number1);
 
                 if (number % number1 == 0)
                 {
                 Console.WriteLine("Кратно");
                 }
                 else 
-                Console.WriteLine("Некратно, остаток " + number % number1);
+                Console.WriteLine($"Некратно, остаток равен {number % number1}");
