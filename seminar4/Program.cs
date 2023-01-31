2badb5fbc1da42a9148f82e059873dbea3c56317
Viktor Titovich <raffityinet@gmail.com>
2023-01-31 14:09:37 +0300

    Add code

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+
+Console.WriteLine("Введите число: ");
 
     int.TryParse(Console.ReadLine(), out int number);
 
@@ -8,3 +14,48 @@
                 }
                 else 
                 Console.WriteLine("Нет");
+
+
+
+// Даны числа 2 и 3. Найти суммк первых 5 чисел, которые кратны и 2 и 3.
+
+// 1 вариант
+
+// int a = 1;
+// int count = 0;
+// int sum = 0;
+// while (count < 5)
+// {
+//     if ((a % 2 == 0) && (a % 3 == 0))
+//     {
+//         Console.WriteLine(a);
+//         sum = sum + a;
+//         count++;
+//     }
+//     a++;
+// }
+// Console.WriteLine (sum);
+
+// 2 вариант неверно работатет, разобраться.
+
+// int a = 3; 
+// int count = 0;
+// int sum = 0;
+// while (count < 5)
+// {
+//     if ((a % 2 == 0) && (a % 3 == 0))
+//     {
+//         Console.WriteLine(a);
+//         sum = sum + a;
+//         count++;
+//     }
+//     a = a * 2;
+// }
+// Console.WriteLine (sum);
+
+//Квадратик
+// Console.WriteLine(@"* * * * *");
+// Console.WriteLine(@"*       *");
+// Console.WriteLine(@"*       *");
+// Console.WriteLine(@"*       *");
+// Console.WriteLine(@"* * * * *");
