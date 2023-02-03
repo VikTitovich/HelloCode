d1b3d956aaea7e23ae393179bcf497a252362e70
Viktor Titovich <raffityinet@gmail.com>
2023-02-03 15:53:09 +0300

    Task done

---
+++ seminar9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3.4 Напишите программу, которая принимает на вход число N и выдаёт таблицу квадратов чисел от 1 до N
+
+System.Console.WriteLine("Введите число");
+
+int.TryParse(Console.ReadLine(), out int a);
+
+int b = 1;
+
+while(b <= a)
+{
+    System.Console.WriteLine(Math.Pow(b, 2));
+    b++;
+}
