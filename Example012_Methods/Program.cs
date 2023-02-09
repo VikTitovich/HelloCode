4cc039fdc9e1c04ffc76f14dfa5bbfe6e982b9ea
Viktor Titovich <raffityinet@gmail.com>
2023-02-09 16:33:09 +0300

    example012

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+///В тексте заменить все пробелы черточками, маленькие к большими К, а большие С заменить
+// маленькими c/
+
+string text = "опопопопоп  лллккк   сссс";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue)
+        {
+            result = result + $"{newValue}";
+        }
+        else
+        {
+            result = result + $"{text[i]}";
+        }
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+
+
+
